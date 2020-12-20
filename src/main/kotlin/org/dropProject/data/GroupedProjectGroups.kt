b51ee939e52c204ee16267a1050b3d1ab6366b60
Bruno Cipriano <bcipriano@gmail.com>
2020-12-20 23:17:19 +0000

    Improve documentation of the GroupedProjectGroups class

--- src/main/kotlin/org/dropProject/data/GroupedProjectGroups.kt
+++ src/main/kotlin/org/dropProject/data/GroupedProjectGroups.kt
@@ -3,12 +3,10 @@ package org.dropProject.data
 import org.apache.coyote.http11.Constants.a
 import org.dropProject.dao.ProjectGroup
 
-//class GroupedGroups(ids : List<Long>, failedTestNames : List<String>) {
-
 /**
  * Represents student groups that were signalled as failling exactly the same unit tests
- * @property groups is a List of ProjectGroups
- * @property failedTestNames is a List of String
+ * @property groups is a List of [ProjectGroup]s
+ * @property failedTestNames is a List of Strings with the names of the unit tests that the groups are failing
  */
 data class GroupedProjectGroups(val groups : List<ProjectGroup>,
                                 val failedTestNames: List<String>) {
